751fe70df9c9e027c9417e7ac2da63e33aefce2b
karthiknInfinity45 <karthikeyan.s@roundrockabaschool.onmicrosoft.com>
2024-04-18 18:38:40 +0530

    get-ordered-item-api

--- src/routes/RestaurentRoute.ts
+++ src/routes/RestaurentRoute.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { jwtCheck, jwtParse } from '../middlewares/auth';
 import multer from 'multer';
-import { CreateMyRestaurant, getMyRestaurant, getRestaurantItems, searchRestaurants, updateMyRestaurant } from '../controllers/RestaurentController';
+import { CreateMyRestaurant, getMyRestaurant, getRestaurantItems, searchRestaurants, updateMyRestaurant, getMyRestaurantOrders, updateOrderStatus } from '../controllers/RestaurentController';
 import { validateRestaurant } from '../middlewares/validation';
 import { param } from 'express-validator';
 
@@ -16,9 +16,11 @@ const upload = multer({
     }
 })
 // jwtCheck, jwtParse,
+router.route('/order').get(jwtCheck,jwtParse, getMyRestaurantOrders)
 router.route('/getMyRestaurant').get(jwtCheck,jwtParse, getMyRestaurant)
 router.route('/createMyRestaurant').post(jwtCheck, jwtParse, upload.single('imageFile'),validateRestaurant,  CreateMyRestaurant)
 router.route('/updateMyRestaurant').put(jwtCheck, jwtParse, upload.single('imageFile'),validateRestaurant,  updateMyRestaurant)
 router.route("/search/:city").get(param("city").isString().trim().notEmpty().withMessage("City parameter must be a valid string"),searchRestaurants)
 router.route("/:restaurantId").get(param("restaurantId").isString().trim().notEmpty().withMessage("RestaurantId parameter must be a valid string"), getRestaurantItems)
+router.route("/order/:orderId").patch(jwtCheck,jwtParse, updateOrderStatus)
 export default router;
